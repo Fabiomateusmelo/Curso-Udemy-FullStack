f296e47cfbe1bc485c4a650fff8e9dbca4bf7e8a
Fábio Mateus Melo <fabioceto@gmail.com>
2023-02-17 17:12:38 -0300

    0003

---
+++ formulario.js
@@ -0,0 +1,30 @@
+function meuEscopo () {
+    const form = document.querySelector('.form')
+    const resultado = document.querySelector('.resultado')
+    /*form.onsubmit = function (evento) {
+        evento.preventDefault()
+        alert(1)
+        console.log('Foi enviado')
+    };*/
+
+    // Outra maneira de controlar um evento é com o addEventListener, nesse caso impede que a página recarregue como o padrão:
+    /*let contador = 1
+    function recebeEventoForm (evento) {
+        evento.preventDefault()
+        console.log(`Form não foi enviado ${contador}`)
+        contador++
+    }
+    form.addEventListener('submit', recebeEventoForm)*/
+
+    function recebeEventoForm (evento) {
+        evento.preventDefault();
+        const nome = form.querySelector('.nome')
+        const sobrenome = form.querySelector('.sobrenome')
+        const peso = form.querySelector('.peso')
+        const altura = form.querySelector('.altura')
+
+        console.log(nome.value, sobrenome, peso, altura)
+    }
+    form.addEventListener('submit', recebeEventoForm)
+}
+meuEscopo()
